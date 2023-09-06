3a518b4eb0dcbb4ab93f817c0e46d5a75483a1d1
Carlos Tavares <tav.carlos1998@gmail.com>
2023-09-06 19:06:20 -0300

    criado entidade usuario e seu: repositorio, service e controller

---
+++ src/main/java/com/demoparkapi/entity/Usuario.java
@@ -0,0 +1,65 @@
+package com.demoparkapi.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.demoparkapi.enums.Role;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "username", nullable = false, unique = true, length = 200)
+	private String username;
+	
+	@Column(name = "password", nullable = false, length = 200)
+	private String password;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false, length = 25)
+	private Role role;
+	
+	@Column(name = "creation_date")
+	private LocalDateTime creationDate;
+	
+	@Column(name = "modification_date")
+	private LocalDateTime modificationDate;
+	
+	@Column(name = "created_by")
+	private String createdBy;
+	
+	@Column(name = "modified_by")
+	private String modifiedBy;
+
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
---
+++ src/main/java/com/demoparkapi/enums/Role.java
@@ -0,0 +1,6 @@
+package com.demoparkapi.enums;
+
+public enum Role {
+
+	ROLE_ADMIN, ROLE_CLIENT
+}
